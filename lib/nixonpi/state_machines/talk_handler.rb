229f844798c9472b7d606b500454cb2ed192cfd2
Daniel Kummer <daniel.kummer@gmail.com>
2012-11-26 20:49:06 +0100

    add tts support on osx and logging on all

--- lib/nixonpi/state_machines/talk_handler.rb
+++ lib/nixonpi/state_machines/talk_handler.rb
@@ -3,15 +3,27 @@ require 'festivaltts4r'
 require_relative '../command_queue'
 require_relative '../control_parameters'
 require_relative 'handler_state_machine'
+require_relative '../../os'
+require_relative '../logging/logging'
 
 
 module NixieBerry
   class TalkHandler < HandlerStateMachine
+    include OS
+    include Logging
 
     register_as :say
 
     def write
-      current_state_parameters[:value].to_speech
+      log.debug "Say: #{current_state_parameters[:value].to_speech}"
+      case true
+        when OS.mac?
+          IO.popen("say #{current_state_parameters[:value]}")
+        when OS.windows?
+          log.warn "No windows speech support at the moment..."
+        else
+          current_state_parameters[:value].to_speech
+      end
     end
   end
 end
