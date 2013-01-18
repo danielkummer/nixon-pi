a3c8ea22a1bd75ea3d11b79206f8b21e36cf9841
Daniel Kummer <daniel.kummer@gmail.com>
2013-01-18 23:43:39 +0100

    fix speech driver

---
+++ lib/nixonpi/drivers/speech_driver.rb
@@ -0,0 +1,53 @@
+require 'singleton'
+require_relative 'driver'
+require_relative '../configurations/settings'
+
+module NixonPi
+  class PowerDriver
+    include Logging
+    include Singleton
+    include Driver
+
+    def initialize
+      @power_pin = Settings.power_pin
+      @value = 0
+      log.info "Initializing power pin: #{@power_pin.to_s}"
+      CommandProcessor.add_receiver(self, :power)
+    end
+
+    def receive(command)
+      value = command[:value]
+      log.debug "got power command: #{command}, applying..."
+      if (0..1).member?(value)
+        client.io_write(@power_pin, value)
+        @value = value
+      end
+    end
+
+    def power_on
+      @value = 1
+      client.io_write(@power_pin, 1)
+    end
+
+    def power_off
+      @value = 0
+      client.io_write(@power_pin, 0)
+    end
+
+    # Currently unused..
+    def write(power_on = false)
+      write = power_on ? 1 : 0
+      client.io_write(@power_pin, write)
+      @value = write
+    end
+
+    def on?
+      @value == 1 ? true : false
+    end
+
+    def get_params
+      {value: @value}
+    end
+
+  end
+end
