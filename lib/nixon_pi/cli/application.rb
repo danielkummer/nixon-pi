0113008a564a6af2d878ba948412e79b205378a1
Daniel Kummer <daniel.kummer@gmail.com>
2015-05-23 14:05:36 +0200

    add --db to cli

--- lib/nixon_pi/cli/application.rb
+++ lib/nixon_pi/cli/application.rb
@@ -14,14 +14,18 @@ module NixonPi
             With -p option, you can supply a port for the webservice
 
             With --environment option, the environment can be specified - development, production
+
+            With --db option,  set the path for the database, the file nixon_pi.sqlite3 will be stored there
       LONGDESC
       option :env, default: 'development', banner: '<environment>'
       option :m, type: :boolean, banner: 'Use telnet mock implementation', default: false, tags: :boolean
       option :p, type: :numeric, banner: 'Set webserver port', default: 8080
+      option :db, type: :string, banner: '<path>'
       desc 'service', 'starts the service'
       def service
         $environment = options[:env]
         NixonPi::Settings.reload! # make sure we have the correct settings for the correct environment!
+        NixonPi::Settings['database_path'] = options[:db] if options[:db]
         NixonPi::Settings['force_mock'] = true if options[:m]
         NixonPi::Runner.new.run(options)
       end
@@ -32,10 +36,13 @@ module NixonPi
             With -p option, you can supply a port for the webservice
 
             With --environment option, the environment can be specified - development, production
+
+            With --db option,  set the path for the database, the file nixon_pi.sqlite3 will be stored there
       LONGDESC
       desc 'web', 'start web server instance using thin'
       option :env, default: 'development', banner: '<environment>'
       option :p, type: :numeric, banner: 'Set webserver port', default: 8080
+      option :db, type: :string, banner: '<path>'
       def web
         $environment = options[:env]
 
